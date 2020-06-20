8e80e8b6703fa005866effefdbbd5bcab4f1ebdd
Pelle Pelster <pelle@pelle.io>
2020-06-21 00:00:22 +0200

    add missing attributes for zone response

--- src/main/java/io/pelle/hetzner/model/ZoneResponse.java
+++ src/main/java/io/pelle/hetzner/model/ZoneResponse.java
@@ -1,8 +1,47 @@
 package io.pelle.hetzner.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 public class ZoneResponse {
+
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Data
+  private class TxtVerification {
+    private String name;
+    private String token;
+  }
+
   private String id;
+  private String name;
+  private String owner;
+  private boolean paused;
+  private String permission;
+  private String project;
+  private String registrar;
+  private String status;
+  private Integer ttl;
+  private String created;
+  private String modified;
+  private String verified;
+
+  @JsonProperty("legacy_dns_host")
+  private String legacyDnsHost;
+
+  @JsonProperty("legacy_ns")
+  private List<String> legacyNs;
+
+  @JsonProperty("records_count")
+  private Integer recordscount;
+
+  @JsonProperty("is_secondary_dns")
+  private boolean isSecondaryDns;
+
+  @JsonProperty("txt_verification")
+  private TxtVerification txtVerification;
 }
--- src/test/java/io/pelle/hetzner/HetznerDnsAPIIntegrationTest.java
+++ src/test/java/io/pelle/hetzner/HetznerDnsAPIIntegrationTest.java
@@ -17,7 +17,7 @@ public class HetznerDnsAPIIntegrationTest {
     var dnsApi = new HetznerDnsAPI(System.getenv("INTEGRATION_TEST_API_TOKEN"));
 
     // deleting invalid zone id is handles gracefully
-    assertThat(dnsApi.deleteZone("6JjmDvFcySLaMsoTNyqrt4"), is(false));
+    assertThat(dnsApi.deleteZone("XXXXXXXSLaMsoTNyqrt4"), is(false));
 
     // clean up from previous tests if needed
     dnsApi
@@ -27,16 +27,16 @@ public class HetznerDnsAPIIntegrationTest {
     var totalZones = dnsApi.getZones().size();
 
     assertThat(dnsApi.searchZone(INTEGRATION_TEST_ZONE_NAME).isPresent(), is(false));
-
     dnsApi.createZone(ZoneCreateRequest.builder().name(INTEGRATION_TEST_ZONE_NAME).build());
 
-    assertThat(dnsApi.searchZone(INTEGRATION_TEST_ZONE_NAME).isPresent(), is(true));
+    var zone = dnsApi.searchZone(INTEGRATION_TEST_ZONE_NAME);
+
+    assertThat(zone.isPresent(), is(true));
+    assertThat(zone.get().getName(), is(INTEGRATION_TEST_ZONE_NAME));
 
     assertThat(dnsApi.getZones().size(), is(totalZones + 1));
 
     // clean up test zone
-    dnsApi
-        .searchZone(INTEGRATION_TEST_ZONE_NAME)
-        .ifPresent(zone -> dnsApi.deleteZone(zone.getId()));
+    dnsApi.searchZone(INTEGRATION_TEST_ZONE_NAME).ifPresent(z -> dnsApi.deleteZone(z.getId()));
   }
 }
